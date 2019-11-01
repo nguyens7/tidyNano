654bb5bd8d743cfd813fd73cc96f07a16690f6b9
nguyens7 <nguyens7@msu.edu>
2019-11-01 17:03:41 -0400

    updated nanoimport and tidyNanov 0.1.2

--- R/nanoimport.R
+++ R/nanoimport.R
@@ -2,9 +2,6 @@
 #'
 #' This function allows you aggregate or summarize your data by groups.
 #' @param file A .csv file from a Malvern Nanosight machine.
-#' @param range The range of values (nm) used during NTA data acquisition, defaults to 1000.
-#' @param bin_width The bin width used during NTA data acquisition, defaults to 1.
-#' @param nm_start Starting measurement value (nm), defaults to 0.5nm
 #' @param auto_name Extracts sample and dilution data and appends to column header, defaults to FALSE.
 #' @param custom_name Append custom name to column header, defaults to NULL.
 #' @return Dataframe of NTA data.
@@ -15,60 +12,37 @@
 
 
 nanoimport <- function(file,
-                        range = 1000,
-                        bin_width = 1,
-                        nm_start = 0.5,
                         auto_name = FALSE,
                         custom_name = NULL){
 
   # NTA version
-  NTA_version <- utils::read.csv(file, header = FALSE)[2,1] %>%
-    stringr::str_sub(18,20) %>%
-    as.numeric()
+  NTA_Version <- read.csv(file, header = FALSE)[2,1] %>%
+    stringr::str_sub(18,20)
 
-  message(glue::glue("NTA version: {NTA_version}"))
+  message(glue::glue("NTA version: {NTA_Version}"))
 
   # Sample name
   sample_name <- utils::read.csv(file, header = FALSE, blank.lines.skip = FALSE)[7,2]
 
   message(glue::glue("Sample name: {sample_name}"))
 
-  # Detect number of parameters
-  param <- suppressWarnings(readr::read_csv(file, skip = 71,
-                                            n_max = 20,
-                                            col_types = readr::cols()))[,1] %>%
-    dplyr::rename(data = `[Data Included]`) %>%
-    tidyr::drop_na() %>%
-    dplyr::mutate(num = row_number()) %>%
-    dplyr::filter(data == "[Size Data]") %>%
-    dplyr::select(num)
-
-  param_num <- param$num - 1
-
-  message(glue::glue("Number of parameters detected: {param_num}"))
-
   # Dilution factor
-  dilution <- read.csv(file, header = FALSE, skip = 42, nrows = 1)[,2] %>%
+
+  NTA_text <- readLines(file)
+
+  dilution_line_no <- grep("^\\[Results\\]", NTA_text)
+
+  message(glue::glue("Dilution line no: {dilution_line_no}"))
+
+  dilution <- read.csv(file, header = FALSE, skip = dilution_line_no, nrows = 1)[,2] %>%
     as.numeric()
 
   message(glue::glue("Dilution factor detected: {dilution}"))
 
-  # Import
-  if(NTA_version == 2.3){
+  # Autoname
 
-    header_skip <- 71
-    df_skip <- 73
-    nm_start <- 0
+  if(auto_name == FALSE && is.null(custom_name) ){
 
-  }else if(NTA_version == 3.1){
-
-    header_skip <- 76
-    df_skip <- 86
-
-  }else if(NTA_version == 3.2 && auto_name == FALSE && is.null(custom_name) ){
-
-    header_skip <- 76 + param_num
-    df_skip <- header_skip + 10
     gen_auto_name <- ""
     dilution <- 1
 
@@ -76,10 +50,8 @@ nanoimport <- function(file,
                        Custom name: NULL
                        Dilution value: {dilution} (Didn't parse)"))
 
-  }else if(NTA_version == 3.2 && auto_name == FALSE ){
+  }else if(auto_name == FALSE){
 
-    header_skip <- 76 + param_num
-    df_skip <- header_skip + 10
     gen_auto_name <- paste0("_", custom_name)
     dilution <- 1
 
@@ -87,36 +59,40 @@ nanoimport <- function(file,
                        Custom name: {custom_name}
                        Dilution value: {dilution} (Didn't parse)"))
 
-  }else if(NTA_version == 3.2 && auto_name == TRUE && is.null(custom_name) ){
-    header_skip <- 76 + param_num
-    df_skip <- header_skip + 10
-    gen_auto_name <- paste0("_", sample_name, "_", dilution)
+  }else if(auto_name == TRUE && is.null(custom_name) ){
+
+    gen_auto_name <- base::paste0("_", sample_name, "_", dilution)
 
     message(glue::glue("Auto name: {gen_auto_name}
                        Custom name: NULL
                        Dilution value: {dilution}"))
 
-  }else if(NTA_version == 3.2 && auto_name == TRUE ){
+  }else if(auto_name == TRUE ){
 
-    header_skip <- 76 + param_num
-    df_skip <- header_skip + 10
-    gen_auto_name <- paste0("_", custom_name, "_", sample_name, "_", dilution)
+    gen_auto_name <- base::paste0("_", custom_name, "_", sample_name, "_", dilution)
 
     message(glue::glue("Auto name: {gen_auto_name}
                        Custom name: {custom_name}
                        Dilution value: {dilution}"))
   }
 
-  # Generation of data frame
-  num_rows <- length(seq(nm_start , range, bin_width))
+  # Import data
 
-  header <- read.csv(file, skip = header_skip, header = FALSE,
+  header_skip <- base::grep("^Filename", NTA_text)[2] - 1
+
+  df_skip <- base::grep("^Bin centre", NTA_text)
+
+  df_end <- base::grep("^Percentile", NTA_text) - 2
+
+  num_rows <- df_end - df_skip
+
+  header <- utils::read.csv(file, skip = header_skip, header = FALSE,
                      nrows = 1, as.is = T, stringsAsFactors = FALSE)
 
-  df <-  read.csv(file, skip = df_skip, header = FALSE,
-                  nrows = num_rows, stringsAsFactors = FALSE)
+  df <-  utils::read.csv(file, skip = df_skip, header = FALSE, nrows = num_rows,
+                  stringsAsFactors = FALSE)
 
-  colnames(df) <-  header
+  base::colnames(df) <-  header
 
   df <-  df[!is.na(names(df))] # Remove NA column
 
@@ -124,13 +100,15 @@ nanoimport <- function(file,
     dplyr::rename(particle_size = Filename) %>%
     dplyr::select(particle_size)
 
+  dilute_correction <- function(x){x/dilution}
+
   NTA_data <- df %>%
     dplyr::select(-Filename, -Average, -`Standard Error`) %>%
     dplyr::rename_all( ~ paste0(., gen_auto_name)) %>%
     dplyr::as_tibble() %>%
-    dplyr::mutate_all(funs( . / dilution))
-
+    dplyr::mutate_all(dilute_correction) #adjust dilution factor if entered in during acquisition
+  # nanotidy() will correct for this with the `True_count` column
   part_size %>%
     dplyr::bind_cols(NTA_data)
 
-  }
+}
--- README.md
+++ README.md
@@ -1,35 +1,63 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-tidyNano <img src="https://raw.githubusercontent.com/nguyens7/tidyNano/master/man/figures/tidyNano.png" align="right" alt="" width="120" />
-===========================================================================================================================================
 
-tidyNano is an R package that imports raw NanoSight data and provides a framework to clean, analyze, and visualize nanoparticle analysis data. You can browse the source code on [GitHub](https://github.com/nguyens7/tidyNano).
+# tidyNano <img src="https://raw.githubusercontent.com/nguyens7/tidyNano/master/man/figures/tidyNano.png" align="right" alt="" width="120" />
 
-Check out our manuscript in [PLOS ONE](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0218270)!
+tidyNano is an R package that imports raw NanoSight data and provides a
+framework to clean, analyze, and visualize nanoparticle analysis data.
+You can browse the source code on
+[GitHub](https://github.com/nguyens7/tidyNano).
 
-Overview
-========
+Check out our manuscript in [PLOS
+ONE](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0218270)\!
+
+# News (11/2019)
+
+`nanoimport()` has just been updated to be more flexible and works with
+NTA versions 3.0 - 3.2\! The function should be able to detect and
+import your data.
+
+**Nomenclature**  
+We recommend naming your files in `snake_case` so it’s conducive for
+processing with the `nanotidy()` function. With each sample we recommend
+adding important information so you can parse it out during analysis, be
+sure to influde the dilution value in the sample name if you do not
+enter it during the dilution entry box during acquisition. For example
+you can name your files using the general formula
+`SampleID_Treatment_1500_`. Be sure to leave a `_` at the end so the NTA
+software will append your files with a numerical count (00,01,02…) which
+will serve as way to track your technical replicates.
+
+If you already collected you data and don’t have your dilution in your
+sample name you can simply add the dilution when you use `nanoimport()`.
+Simply add the dilution value in the `custom_name` argument. If you did
+label your samples and enter in the dilution in the NTA software during
+acquisition you can make `auto_name == TRUE` in
+`nanoimport()`.
+
+# Overview
 
 <img src="https://raw.githubusercontent.com/nguyens7/tidyNano/master/man/figures/tidyNano_Schema.png" align="center" width = "600"/>
 
-tidyNano functions (Purple) allow for easily extracting and converting raw NTA count data into a tidy dataframe that is suitable for analysis using dplyr and ggplot2. tidyNano also provides a interactive shiny application (shinySIGHT) for visualizing data.
+tidyNano functions (Purple) allow for easily extracting and converting
+raw NTA count data into a tidy dataframe that is suitable for analysis
+using dplyr and ggplot2. tidyNano also provides a interactive shiny
+application (shinySIGHT) for visualizing
+data.
 
 <img src="https://raw.githubusercontent.com/nguyens7/tidyNano/master/man/figures/tidyNano_workflow.png" align="center" width = "400"/>
 
-Installation
-============
+# Installation
 
 The latest development version can be installed from github:
 
-```r
+``` r
 # install.packages("devtools")
 
-devtools::install_github("nguyens7/tidyNano")
+# devtools::install_github("nguyens7/tidyNano")
 ```
 
-
-\# tidyNano Example
--------------------
+## \# tidyNano Example
 
 ``` r
 library(tidyNano)
