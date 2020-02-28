0634231b8c4f45fc0859a1ea13e2646c87b15782
nguyens7 <nguyens7@msu.edu>
2020-02-28 13:38:54 -0500

    Added sci notation to nanolyze

--- R/nanolyze.R
+++ R/nanolyze.R
@@ -6,6 +6,7 @@
 #' @param name Prefix name to summary statistics
 #' @param param_var The numerical value that you want to summarize.
 #' @param na.rm Argument to ignore 'NA' values, defaults to TRUE.
+#' @param sci_not Argument to return a separate column with scientific notation, defaults to FALSE.
 #' @return Summarized values \code{parm_var} grouped by \code{...}.
 #' Output of tidy dataframe of grouped by \code{...} variables, N, mean,
 #' standard deviation and standard error labeled according to 'name' argument.
@@ -17,25 +18,45 @@
 #' @export
 
 
-nanolyze <- function(df, ..., name = "Param", param_var, na.rm = TRUE) {
+nanolyze <- function(df, ..., name = "Param", param_var, na.rm = TRUE, sci_not = FALSE) {
 
 
   group_var <- dplyr::quos(...)
   param_var <- dplyr::enquo(param_var)
   N <- paste(name, "N" , sep = "_")
-  mean <- paste(name, "mean" , sep = "_")
+  cust_mean <- paste(name, "mean" , sep = "_")
+  sci_cust_mean <- paste(name, "sci_no_mean" , sep = "_")
   sd <- paste(name, "sd" , sep = "_")
   se <- paste(name, "se" , sep = "_")
 
+  if(sci_not == FALSE){
+
   df %>%
-    dplyr::group_by(!!! group_var) %>%
-    dplyr::summarise(   !! N := length(na.omit(!! param_var)),
-              !! mean := mean(!! param_var, na.rm = na.rm),
-                !! sd := sd(!! param_var, na.rm = na.rm),
-                !! se := sd(!! param_var, na.rm = na.rm) /
-                          sqrt(length(na.omit(!!param_var)))
-              ) %>%
-    dplyr::ungroup()
+      dplyr::group_by(!!! group_var) %>%
+      dplyr::summarise(   !! N := length(na.omit(!! param_var)),
+                          !! cust_mean := mean(!! param_var, na.rm = na.rm),
+                          !! sd := sd(!! param_var, na.rm = na.rm),
+                          !! se := sd(!! param_var, na.rm = na.rm) /
+                            sqrt(length(na.omit(!!param_var)))
+      ) %>%
+      dplyr::ungroup()
+
+  }else if (sci_not == TRUE){
+
+    df %>%
+      dplyr::group_by(!!! group_var) %>%
+      dplyr::summarise(   !! N := length(na.omit(!! param_var)),
+                          !! cust_mean := mean(!! param_var, na.rm = na.rm),
+                          !! sci_cust_mean := mean(!! param_var,na.rm = na.rm) %>%
+                                                scales::scientific(digits = 3),
+                          !! sd := sd(!! param_var, na.rm = na.rm),
+                          !! se := sd(!! param_var, na.rm = na.rm) /
+                            sqrt(length(na.omit(!!param_var)))
+      ) %>%
+      dplyr::ungroup()
+
+
+  }
 
     }
 
